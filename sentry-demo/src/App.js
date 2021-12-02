e58e5d3e6445709ff7a083d586b8f511491612a1
Halil Özat <halilozat@hotmail.com>
2021-12-02 15:24:10 +0300

    added new optinos

--- sentry-demo/src/App.js
+++ sentry-demo/src/App.js
@@ -1,16 +1,57 @@
 import './App.css';
+import React, { useState } from 'react';
 import * as Sentry from '@sentry/react';
 
 function App() {
+  const data = [
+    {
+      id: 1,
+      user: 'halil',
+      password: '123',
+    },
+  ];
+
+  const [state, setState] = useState({
+    message: 'This is my app',
+  });
+
+  // const buttonError = () => {
+  //   try {
+  //     if (data.length !== 0) {
+  //       throw new Error('data length error');
+  //     }
+  //   } catch (error) {
+  //     Sentry.captureException(error);
+  //     console.log('error');
+  //     // Sentry.captureMessage('error');
+  //     // Sentry.addBreadcrumb(error);
+  //   }
+  // };
+  console.log(state.message);
+
   return (
-    <button
-      type="button"
-      onClick={() => {
-        throw Error('button error');
-      }}
+    <Sentry.ErrorBoundary
+      fallback={({ error, componentStack, resetError }) => (
+        <React.Fragment>
+          <div>You have encountered an error</div>
+          <div>{error.toString()}</div>
+          <div>{componentStack}</div>
+          <button
+            onClick={() => {
+              setState({ message: 'This is my app' });
+              resetError();
+            }}
+          >
+            Click here to reset!
+          </button>
+        </React.Fragment>
+      )}
     >
-      Break the world
-    </button>
+      <div>{state.message}</div>
+      <button onClick={() => setState({ message: 'Hello World' })}>
+        Click here to change message!
+      </button>
+    </Sentry.ErrorBoundary>
   );
 }
-export default Sentry.withProfiler(App);
+export default App;
