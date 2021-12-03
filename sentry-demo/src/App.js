7bb215613b38e0b470741f10925080404ab6693c
Halil Özat <halilozat@hotmail.com>
2021-12-03 15:44:22 +0300

    added new tags

--- sentry-demo/src/App.js
+++ sentry-demo/src/App.js
@@ -16,11 +16,17 @@ function App() {
         throw new Error('data length error');
       }
     } catch (error) {
-      Sentry.captureException(new Error('something went wrong'), {
-        tags: {
-          section: 'articles',
-        },
-      });
+      // Sentry.captureException(new Error('clean as never'), (scope) => {
+      //   scope.clear();
+      //   scope.setTag('clean', 'slate');
+      //   return scope;
+      // });
+
+      const scope = new Sentry.Scope();
+      scope.setTag('clean', 'slate');
+      scope.setTag('section', 'articles');
+      Sentry.captureException(new Error('something went wronggg'), scope);
+
       console.log('error');
       console.log(data);
       // Sentry.captureMessage('error');
